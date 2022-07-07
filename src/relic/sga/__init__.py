0e3a787b550a91e4129e4ca2e0394485f4e85e68
Marcus Kertesz <ModernMAK@Hotmail.com>
2022-07-06 23:22:16 -0800

    Update README.md, MANIFEST.in, setup.cfg
    Also adds version to relic.sga's __init__.py for universal version truth

--- src/relic/sga/__init__.py
+++ src/relic/sga/__init__.py
@@ -1,9 +1,9 @@
 """
-API for Relic's SGA files
+A library for reading & writing Relic's SGA archive files.
 """
 from relic.sga._apis import apis as APIs, read
 from relic.sga._core import Version, MagicWord, StorageType, VerificationType
-
+__version__ = "2022.1rc0"
 __all__ = [
     "read",
     "APIs",
